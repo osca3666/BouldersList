bfa9cb847768274767b1ae6b516ca2e12b7724ae
githubUsername <emailLinkedtoGithubAcct@email.com>
2023-11-12 19:21:44 +0000

    Added users table to the database schema

---
+++ all-project-code/src/init_data/create.sql
@@ -0,0 +1,11 @@
+
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+ 	password_hash VARCHAR(255) NOT NULL
+);
