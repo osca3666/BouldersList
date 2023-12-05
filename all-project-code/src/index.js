a883194f1b11e05299d3d171913a16e718d63dd4
Moses Williams <moseswilliams@cu-tcom-2-10.203.14.202.int.colorado.edu>
2023-12-05 12:30:00 -0700

    newtestrun

--- all-project-code/src/index.js
+++ all-project-code/src/index.js
@@ -511,7 +511,47 @@ app.get('/get_ratings', (req, res) => {
       });
 });
 
+app.post('/submit-business', async (req, res) => {
+  try {
+    const { businessName, businessDescription, businessImageURL } = req.body;
 
+    // Validate input data if needed
+
+    // For demonstration purposes, I'm assuming you have a static api_business_id, type, and address
+    const api_business_id = 'static-id';
+    const type = 'static-type';
+    const address = 'static-address';
+
+    const query = `
+      INSERT INTO business (api_business_id, name, phone_number, description, image, type, address)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *;`;
+
+    const data = await db.one(query, [api_business_id, businessName, null, businessDescription, businessImageURL, type, address]);
+
+    console.log(data);
+    res.status(201).json({
+      status: 'success',
+      message: 'Business added successfully.',
+      data: {
+        business_id: data.business_id,
+        api_business_id: data.api_business_id,
+        name: data.name,
+        description: data.description,
+        image: data.image,
+        type: data.type,
+        address: data.address,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({
+      status: 'error',
+      message: 'Business upload failed.',
+      error: err.message,
+    });
+  }
+});
 
 
 // starting the server and keeping the connection open to listen for more requests
